b5aebe4a4338e2f0faca9afc468736a6402c6c6b
JankiBhatt1707 <jbhatt17792@gmail.com>
2024-03-07 20:54:06 +0530

    initial commit

---
+++ resources/views/form.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload Roster</title>
+</head>
+<body>
+    <form method="POST" action="/parse-roster" enctype="multipart/form-data">
+        @csrf
+        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
+        <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400
+        focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" name="file">
+        <button type="submit" class="btn-primary">Parse Roster</button>
+    </form>
+</body>
+</html>
+
